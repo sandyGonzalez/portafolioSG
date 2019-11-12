6e1dc4b2c5212c11a0c13471f282feb150f091d8
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-11-12 12:26:07 -0600

    Agregando Cards para el portafolio con imagenes ya es responsive

--- src/app.js
+++ src/app.js
@@ -3,8 +3,4 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Sidenav.init(elems);
   });
 
-  // Or with jQuery
-/* 
-  $(document).ready(function(){
-    $('.sidenav').sidenav();
-  }); */
+ 
